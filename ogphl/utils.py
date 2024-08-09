81d46798a171f49229295a8b833a5005d32d4a88
jdebacker <jason.debacker@gmail.com>
2024-08-09 05:18:30 -0400

    wrap comment

--- ogphl/utils.py
+++ ogphl/utils.py
@@ -6,8 +6,12 @@ import socket
 
 class CustomHttpAdapter(requests.adapters.HTTPAdapter):
     """
-    The UN Data Portal server doesn't support "RFC 5746 secure renegotiation". This causes and error when the client is using OpenSSL 3, which enforces that standard by default.
-    The fix is to create a custom SSL context that allows for legacy connections. This defines a function get_legacy_session() that should be used instead of requests().
+    The UN Data Portal server doesn't support
+    "RFC 5746 secure renegotiation". This causes and error when the
+    client is using OpenSSL 3, which enforces that standard by default.
+    The fix is to create a custom SSL context that allows for legacy
+    connections. This defines a function get_legacy_session() that
+    should be used instead of requests().
     """
 
     # "Transport adapter" that allows us to use custom ssl_context.
