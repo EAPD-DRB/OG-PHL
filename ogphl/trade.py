622cedf4d5f30de0f42c0156075425d9bc745f67
Richard Evans <rickecon@gmail.com>
2024-10-22 11:22:35 +0800

    Added trade.py and updated constants.py

---
+++ ogphl/trade.py
@@ -0,0 +1,13 @@
+"""
+This is the trade module for OG-PHL
+"""
+
+import pandas as pd
+import numpy as np
+import os
+from ogphl.constants import CONS_DICT, PROD_DICT
+
+
+def export_share(imperfect_competition, dev_indic):
+    stuff
+    return exp_share
